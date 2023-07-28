d3be1e6f7af50dab1d85e033f1aacc0f1c0deb23
ASUS <ASUS@LAPTOP-H980KV8D>
2023-07-28 16:08:34 +0530

    First commit

---
+++ C2TC/src/day03/Out.java
@@ -0,0 +1,16 @@
+package day03;
+
+
+
+public class Out {
+
+	public static void main(String[] args) {
+		Student s=new Student();
+		s.setStudid(54);
+		s.setStudname("kiruthika");
+		s.setStuddept("cse");		
+		System.out.println(s.toString());
+
+	}
+
+}
--- C2TC/src/day03/Student.java
+++ C2TC/src/day03/Student.java
@@ -1,10 +1,42 @@
 package day03;
 
 public class Student {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int studid;
+	private String studname;
+	private String studdept;
+	public Student() {
+		super();
+	
 	}
-
+	public Student(int studid, String studname, String studdept) {
+		super();
+		this.studid = studid;
+		this.studname = studname;
+		this.studdept = studdept;
+	}
+	public int getStudid() {
+		return studid;
+	}
+	public void setStudid(int studid) {
+		this.studid = studid;
+	}
+	public String getStudname() {
+		return studname;
+	}
+	public void setStudname(String studname) {
+		this.studname = studname;
+	}
+	public String getStuddept() {
+		return studdept;
+	}
+	public void setStuddept(String studdept) {
+		this.studdept = studdept;
+	}
+	@Override
+	public String toString() {
+		return "Student [studid=" + studid + ", studname=" + studname + ", studdept=" + studdept + "]";
+	}
 }
+	
+	
+	
