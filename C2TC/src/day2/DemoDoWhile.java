746f95725b58a7b53f1cdf1ec918a662901a78c5
ASUS <ASUS@LAPTOP-H980KV8D>
2023-07-29 12:30:13 +0530

    First commit

---
+++ C2TC/src/day2/DemoDoWhile.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class DemoDoWhile {
+		public static void main(String args[])
+		{
+			int i = 1;
+			do {
+				System.out.println("Hi Guys");
+				i++;
+			}
+			while (i < 4);
+		}
+	}
+
+
