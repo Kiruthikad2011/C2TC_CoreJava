9e1616fbfa961353590e1a61724711f74704841d
ASUS <ASUS@LAPTOP-H980KV8D>
2023-07-29 12:41:49 +0530

    First commit

---
+++ C2TC/src/day2/DemoEach.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class DemoEach {
+	  public static void main(String[] args) {
+	    int[] numbers = {3, 9, 5, -5};
+ 
+	    for (int number: numbers) {
+	      System.out.println(number);
+	    }
+	  }
+	}
+
