e22941c5d6e751f5c369ec6e0b5a06b54afcb5c7
ASUS <ASUS@LAPTOP-H980KV8D>
2023-07-29 12:22:54 +0530

    First commit

---
+++ C2TC/src/day2/Demo.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Demo {
+
+		public static void main(String args[])
+		{
+			int i = 1;
+			while (i < 6) {
+				System.out.println("Hello World");
+				i++;
+			}
+		}
+	}
+
+
