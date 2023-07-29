8cbdeefab2add18804919a4d9dd5c5ac95ec3073
ASUS <ASUS@LAPTOP-H980KV8D>
2023-07-29 12:35:15 +0530

    First commit

---
+++ C2TC/src/day2/DemoForLoop.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class DemoForLoop {
+
+	  public static void main(String[] args) {
+	  
+	    int n = 5;
+	    // for loop  
+	    for (int i = 1; i <= n; ++i) {
+	      System.out.println(i);
+	    }
+	  }
+	}
+
