add0264ec13443e72e66f87f95d2aace3b80b73f
ASUS <ASUS@LAPTOP-H980KV8D>
2023-08-01 09:35:09 +0530

    First commit

---
+++ C2TC/src/day6/FinalMethod.java
@@ -0,0 +1,17 @@
+package day6;
+
+public class FinalMethod {
+	
+	    public final void display() {
+	      System.out.println("This is a final method.");
+	    }
+	
+
+	
+	  public static void main(String[] args) {
+		  FinalMethod obj = new FinalMethod();
+	    obj.display();
+	  }
+	}
+
+
